aad401ad11115805fd3abde208dd23748aecaac8
Jordan Wu <jordanwu@Jordans-MacBook-Pro-3.local>
2022-02-13 15:31:47 -0700

    add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,12 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
 import type { MetaFunction } from "remix";
 
+import styles from "./tailwind.css";
+
+export function links() {
+	return [{ rel: "stylesheet", href: styles }];
+}
+
 export const meta: MetaFunction = () => {
 	return { title: "True Stories" };
 };
