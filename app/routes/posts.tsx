1cb519ba673628e082a45dd8688e4c46c0ab9ae7
Jordan Wu <jordanwu@Jordans-MacBook-Pro-3.local>
2022-08-19 22:28:53 -0600

    fix width

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -2,10 +2,8 @@ import { Outlet } from "@remix-run/react";
 
 export default function Posts() {
 	return (
-		<div className="flex justify-center">
-			<article className="p-[16px] prose lg:prose-xl">
-				<Outlet />
-			</article>
-		</div>
+		<article className="m-auto p-[16px] prose lg:prose-xl">
+			<Outlet />
+		</article>
 	);
 }
